c8738867a311f5f55ec6792a4051ba6bbb85f861
titaanc <97565183+titaanc@users.noreply.github.com>
2022-01-28 03:36:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,91 +3,216 @@
 function giveMeWord_Ofucada(word) {
   word_end = "";
   switch (word) {
-    case "a":
-      word_end = "00"
-      break;
-      case "b":
-        word_end = "10"
-        break;
-        case "c":
-          word_end = "01"
-          break;
-          case "d":
-            word_end = "11"
-            break;
-            case "e":
-              word_end = "02"
-              break;
-              case "f":
-                word_end = "20"
-                break;
-                case "g":
-                  word_end = "22"
-                  break;
-                  case "h":
-                    word_end = "03"
-                    break;
-                    case "i":
-                      word_end = "30"
-                      break;
-                      case "j":
-                        word_end = "33"
-                        break;
-                        case "k":
-                          word_end = "04"
-                          break;
-                          case "l":
-                            word_end = "40"
-                            break;
-                            case "m":
-                              word_end = "44"
-                              break;
-                              case "n":
-                                word_end = "05"
-                                break;
-                                case "ñ":
-                                  word_end = "50"
-                                  break;
-                                  case "o":
-                                    word_end = "55"
-                                    break;
-                                    case "p":
-                                      word_end = "60"
-                                      break;
-                                      case "q":
-                                        word_end = "06"
-                                        break;
-                                        case "r":
-                                          word_end = "66"
-                                          break;
-                                          case "s":
-                                            word_end = "70"
-                                            break;
-                                            case "t":
-                                              word_end = "07"
-                                              break;
-                                              case "u":
-                                                word_end = "77"
-                                                break;
-                                                case "v":
-                                                  word_end = "80"
-                                                  break;
-                                                  case "w":
-                                                    word_end = "08"
-                                                    break;
-                                                    case "x":
-                                                      word_end = "88"
-                                                      break;
-                                                      case "y":
-                                                        word_end = "90"
-                                                        break;
-                                                        case "z":
-                                                          word_end = "09"
-                                                          break;
-                                                          case " ":
-                                                            word_end = "99"
-                                                            break;
-
+  case "a":
+  word_end = "00"
+  break;
+  case "b":
+  word_end = "10"
+  break;
+  case "c":
+  word_end = "01"
+  break;
+  case "d":
+  word_end = "11"
+  break;
+  case "e":
+  word_end = "02"
+  break;
+  case "f":
+  word_end = "20"
+  break;
+  case "g":
+  word_end = "22"
+  break;
+  case "h":
+  word_end = "03"
+  break;
+  case "i":
+  word_end = "30"
+  break;
+  case "j":
+  word_end = "33"
+  break;
+  case "k":
+  word_end = "04"
+  break;
+  case "l":
+  word_end = "40"
+  break;
+  case "m":
+  word_end = "44"
+  break;
+  case "n":
+  word_end = "05"
+  break;
+  case "ñ":
+  word_end = "50"
+  break;
+  case "o":
+  word_end = "55"
+  break;
+  case "p":
+  word_end = "60"
+  break;
+  case "q":
+  word_end = "06"
+  break;
+  case "r":
+  word_end = "66"
+  break;
+  case "s":
+  word_end = "70"
+  break;
+  case "t":
+  word_end = "07"
+  break;
+  case "u":
+  word_end = "77"
+  break;
+  case "v":
+  word_end = "80"
+  break;
+  case "w":
+  word_end = "08"
+  break;
+  case "x":
+  word_end = "88"
+  break;
+  case "y":
+  word_end = "90"
+  break;
+  case "z":
+  word_end = "09"
+  break;
+  case " ":
+  word_end = "99"
+  break;
+  case "A":
+  word_end = "91"
+  break;
+  case "B":
+  word_end = "19"
+  break;
+  case "C":
+  word_end = "92"
+  break;
+  case "D":
+  word_end = "29"
+  break;
+  case "E":
+  word_end = "93"
+  break;
+  case "F":
+  word_end = "39"
+  break;
+  case "G":
+  word_end = "94"
+  break;
+  case "H":
+  word_end = "49"
+  break;
+  case "I":
+  word_end = "95"
+  break;
+  case "J":
+  word_end = "59"
+  break;
+  case "K":
+  word_end = "96"
+  break;
+  case "L":
+  word_end = "69"
+  break;
+  case "M":
+  word_end = "97"
+  break;
+  case "N":
+  word_end = "79"
+  break;
+  case "O":
+  word_end = "98"
+  break;
+  case "P":
+  word_end = "89"
+  break;
+  case "Q":
+  word_end = "81"
+  break;
+  case "R":
+  word_end = "18"
+  break;
+  case "S":
+  word_end = "82"
+  break;
+  case "T":
+  word_end = "28"
+  break;
+  case "U":
+  word_end = "83"
+  break;
+  case "V":
+  word_end = "38"
+  break;
+  case "W":
+  word_end = "84"
+  break;
+  case "X":
+  word_end = "48"
+  break;
+  case "Y":
+  word_end = "85"
+  break;
+  case "Z":
+  word_end = "58"
+  break;
+  case ":":
+  word_end = "86"
+  break;
+  case "/":
+  word_end = "68"
+  break;
+  case ".":
+  word_end = "87"
+  break;
+  case "-":
+  word_end = "78"
+  break;
+  case "_":
+  word_end = "71"
+  break;
+  case "1":
+  word_end = "17"
+  break;
+  case "2":
+  word_end = "72"
+  break;
+  case "3":
+  word_end = "27"
+  break;
+  case "4":
+  word_end = "73"
+  break;
+  case "5":
+  word_end = "37"
+  break;
+  case "6":
+  word_end = "74"
+  break;
+  case "7":
+  word_end = "47"
+  break;
+  case "8":
+  word_end = "75"
+  break;
+  case "9":
+  word_end = "57"
+  break;
+  case "0":
+  word_end = "76"
+  break;
+  case "?":
+  word_end = "67"
+  break;
 
     default:
       break;
@@ -96,92 +221,217 @@ function giveMeWord_Ofucada(word) {
 }
 function giveMeWord_deOfucada(word) {
   word_end = "";
-  switch (word) {
-    case "00":
-      word_end = "a"
-      break;
-      case "10":
-        word_end = "b"
-        break;
-        case "01":
-          word_end = "c"
-          break;
-          case "11":
-            word_end = "d"
-            break;
-            case "02":
-              word_end = "e"
-              break;
-              case "20":
-                word_end = "f"
-                break;
-                case "22":
-                  word_end = "g"
-                  break;
-                  case "03":
-                    word_end = "h"
-                    break;
-                    case "30":
-                      word_end = "i"
-                      break;
-                      case "33":
-                        word_end = "j"
-                        break;
-                        case "04":
-                          word_end = "k"
-                          break;
-                          case "40":
-                            word_end = "l"
-                            break;
-                            case "44":
-                              word_end = "m"
-                              break;
-                              case "05":
-                                word_end = "n"
-                                break;
-                                case "50":
-                                  word_end = "ñ"
-                                  break;
-                                  case "55":
-                                    word_end = "o"
-                                    break;
-                                    case "60":
-                                      word_end = "p"
-                                      break;
-                                      case "06":
-                                        word_end = "q"
-                                        break;
-                                        case "66":
-                                          word_end = "r"
-                                          break;
-                                          case "70":
-                                            word_end = "s"
-                                            break;
-                                            case "07":
-                                              word_end = "t"
-                                              break;
-                                              case "77":
-                                                word_end = "u"
-                                                break;
-                                                case "80":
-                                                  word_end = "v"
-                                                  break;
-                                                  case "08":
-                                                    word_end = "w"
-                                                    break;
-                                                    case "88":
-                                                      word_end = "x"
-                                                      break;
-                                                      case "90":
-                                                        word_end = "y"
-                                                        break;
-                                                        case "09":
-                                                          word_end = "z"
-                                                          break;
-                                                          case "99":
-                                                            word_end = " "
-                                                            break;
-
+switch (word) {
+  case "00":
+  word_end = "a"
+  break;
+  case "10":
+  word_end = "b"
+  break;
+  case "01":
+  word_end = "c"
+  break;
+  case "11":
+  word_end = "d"
+  break;
+  case "02":
+  word_end = "e"
+  break;
+  case "20":
+  word_end = "f"
+  break;
+  case "22":
+  word_end = "g"
+  break;
+  case "03":
+  word_end = "h"
+  break;
+  case "30":
+  word_end = "i"
+  break;
+  case "33":
+  word_end = "j"
+  break;
+  case "04":
+  word_end = "k"
+  break;
+  case "40":
+  word_end = "l"
+  break;
+  case "44":
+  word_end = "m"
+  break;
+  case "05":
+  word_end = "n"
+  break;
+  case "50":
+  word_end = "ñ"
+  break;
+  case "55":
+  word_end = "o"
+  break;
+  case "60":
+  word_end = "p"
+  break;
+  case "06":
+  word_end = "q"
+  break;
+  case "66":
+  word_end = "r"
+  break;
+  case "70":
+  word_end = "s"
+  break;
+  case "07":
+  word_end = "t"
+  break;
+  case "77":
+  word_end = "u"
+  break;
+  case "80":
+  word_end = "v"
+  break;
+  case "08":
+  word_end = "w"
+  break;
+  case "88":
+  word_end = "x"
+  break;
+  case "90":
+  word_end = "y"
+  break;
+  case "09":
+  word_end = "z"
+  break;
+  case "99":
+  word_end = " " // ESPACIO
+  break;
+  case "91":
+  word_end = "A"
+  break;
+  case "19":
+  word_end = "B"
+  break;
+  case "92":
+  word_end = "C"
+  break;
+  case "29":
+  word_end = "D"
+  break;
+  case "93":
+  word_end = "E"
+  break;
+  case "39":
+  word_end = "F"
+  break;
+  case "94":
+  word_end = "G"
+  break;
+  case "49":
+  word_end = "H"
+  break;
+  case "95":
+  word_end = "I"
+  break;
+  case "59":
+  word_end = "J"
+  break;
+  case "96":
+  word_end = "K"
+  break;
+  case "69":
+  word_end = "L"
+  break;
+  case "97":
+  word_end = "M"
+  break;
+  case "79":
+  word_end = "N"
+  break;
+  case "98":
+  word_end = "O"
+  break;
+  case "89":
+  word_end = "P"
+  break;
+  case "81":
+  word_end = "Q"
+  break;
+  case "18":
+  word_end = "R"
+  break;
+  case "82":
+  word_end = "S"
+  break;
+  case "28":
+  word_end = "T"
+  break;
+  case "83":
+  word_end = "U"
+  break;
+  case "38":
+  word_end = "V"
+  break;
+  case "84":
+  word_end = "W"
+  break;
+  case "48":
+  word_end = "X"
+  break;
+  case "85":
+  word_end = "Y"
+  break;
+  case "58":
+  word_end = "Z"
+  break;
+  case "86":
+  word_end = ":"
+  break;
+  case "68":
+  word_end = "/"
+  break;
+  case "87":
+  word_end = "."
+  break;
+  case "78":
+  word_end = "-"
+  break;
+  case "71":
+  word_end = "_"
+  break;
+  case "17":
+  word_end = "1"
+  break;
+  case "72":
+  word_end = "2"
+  break;
+  case "27":
+  word_end = "3"
+  break;
+  case "73":
+  word_end = "4"
+  break;
+  case "37":
+  word_end = "5"
+  break;
+  case "74":
+  word_end = "6"
+  break;
+  case "47":
+  word_end = "7"
+  break;
+  case "75":
+  word_end = "8"
+  break;
+  case "57":
+  word_end = "9"
+  break;
+  case "76":
+  word_end = "0"
+  break;
+  case "67":
+  word_end = "?"
+  break;
 
     default:
       break;
